13498b093a8a745902ff13310177af736661bad3
nalf3in <nalf3in@gmail.com>
2018-02-15 16:13:57 -0500

    Ajout de la feuille fxml dans le controlleur de la vue

--- src/vue/ControlleurVue.java
+++ src/vue/ControlleurVue.java
@@ -1,7 +1,10 @@
 package vue;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.jme3.material.Material;
 import com.jme3.system.AppSettings;
@@ -11,6 +14,8 @@ import com.jme3x.jfx.injfx.JmeToJFXIntegrator;
 import controleur.Controleur;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -25,6 +30,25 @@ public class ControlleurVue {
 	private Controleur controleurPrincipal;
 
 	public ControlleurVue() {
+		try {
+			// Création du loader.
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vue/FxmlVue.fxml"));
+
+			// Associer le contrôleur
+			fxmlLoader.setController(this);
+
+			// Chargement du fichier FXML
+			Parent root = fxmlLoader.load();
+
+			scene = new Scene(root);
+
+			// TODO Ajouter la feuille de style
+			// attacher la feuille de style
+			// scene.getStylesheets().add(getClass().getResource("/style/Pourboire.css").toString());
+
+		} catch (IOException ex) {
+			System.out.println("Exception lors du chargement des ressources dans controlleur vue");
+		}
 
 	}
 
